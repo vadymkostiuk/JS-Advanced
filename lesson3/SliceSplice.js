7f318bffbeff4af120409ee3b92edb0212377063
vadymkostyuk <kostiukvadym22@gmail.com>
2020-12-19 04:09:44 +0200

    add lesson3

---
+++ lesson3/SliceSplice.js
@@ -0,0 +1,35 @@
+// You are given two arrays and an index.
+
+// Copy each element of the first array into the second array, in order.
+
+// Begin inserting elements at index n of the second array.
+
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+// Method1
+
+function frankenSplice(arr1, arr2, n) {
+  var newArr = arr2.slice();
+
+  newArr.splice(n, 0, ...arr1);
+ 
+  return newArr;
+ 
+}
+
+frankenSplice([1, 2, 3], [4, 5], 1);
+
+// Method2
+
+function frankenSplice(arr1, arr2, n) {
+let newArr = arr2.slice();
+
+for (let i = 0; i < arr1.length; i++) {
+  newArr.splice(n, 0, arr1[i])
+  n++
+}
+
+return newArr
+}
+
+frankenSplice([1, 2, 3], [4, 5], 1);
