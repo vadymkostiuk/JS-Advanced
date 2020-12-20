a7b7852321028d899020a987a72fd25deec3ee73
vadymkostyuk <kostiukvadym22@gmail.com>
2020-12-20 21:14:02 +0200

    Changed lesson2/weather
    Add recognition

--- lesson2/weather.js
+++ lesson2/weather.js
@@ -5,14 +5,14 @@ var windSpeed = document.getElementById('wind-speed');
 var weatherSummary = document.getElementById('weather-summary');
 var weatherSummaryCity = document.getElementById('weather-summary-city');
 var getWeatherButton = document.getElementById('get-weather');
-var weatherCity = document.getElementById("get-weather-city");
 var humidityCity = document.getElementById('humidity-city');
 var pressureCity = document.getElementById('pressure-city');
 var temperatureCity = document.getElementById('temperature-city');
 var windSpeedCity = document.getElementById('wind-speed-city');
 var getWeatherButtonCity = document.getElementById('get-weather-city');
-var curentWeather = document.getElementById('current-weather');
-var curentWeatherCity = document.getElementById('current-weather-city');
+var currentWeather = document.getElementById('current-weather');
+var currentWeatherCity = document.getElementById('current-weather-city');
+var cityWeatherName = document.getElementById("city-weather-name");
 
 function getLocationCoords() {
 
@@ -38,24 +38,24 @@ function getLocationCoordsCity() {
 
 function getWeatherData(latitude, longitude) {
 
-    getFetch =  fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=08d1316ba8742c08076e7425c28c2614')
-    .then(function (response) {
+    fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=08d1316ba8742c08076e7425c28c2614')
+        .then(function (response) {
             return response.json();
-    })
-    .then(function (data) {
+        })
+        .then(function (data) {
             displayData(data);
     });
 
 }
 function getWeatherDataCity() {
-    var etc = document.getElementById("city-weather-name").value;
+    var cityWeatherNameValue = cityWeatherName.innerText;
     
-    getFetch = fetch('http://api.openweathermap.org/data/2.5/weather?q='+ etc + '&appid=08d1316ba8742c08076e7425c28c2614')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        displayDataCity(data);
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+ cityWeatherNameValue + '&appid=08d1316ba8742c08076e7425c28c2614')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            displayDataCity(data);
     });
 
 }
@@ -66,7 +66,7 @@ function displayData(data) {
     temperature.innerText = 'Temperature: ' + kToC(data.main.temp) + ' °C';
     windSpeed.innerText = 'Wind Speed: ' + data.wind.speed + ' m/s';
     weatherSummary.innerText = data.name;
-    curentWeather.innerText = data.weather[0].main;
+    currentWeather.innerText = data.weather[0].main;
     console.log(data);
 }
 
@@ -81,7 +81,7 @@ function displayDataCity(data) {
     temperatureCity.innerText = 'Temperature: ' + kToC(data.main.temp) + ' °C';
     windSpeedCity.innerText = 'Wind Speed: ' + data.wind.speed + ' m/s';
     weatherSummaryCity.innerText = data.name;
-    curentWeatherCity.innerText = data.weather[0].main;
+    currentWeatherCity.innerText = data.weather[0].main;
     console.log(data);
 }
 
@@ -92,3 +92,34 @@ function kToC(k) {
 
 getWeatherButton.addEventListener('click', getLocationCoords);
 getWeatherButtonCity.addEventListener('click', getLocationCoordsCity);
+
+// SpeechRecognition
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+recognition.lang = 'ru-RU';
+
+recognition.addEventListener('result', function (event) {
+
+    var innerResult = Array
+      .from(event.results)
+      .map(function (result) {
+          return result[0];
+      })
+      .map(function (result) {
+          return result.transcript;
+      });
+
+    innerResult[0].toLowerCase();
+
+    if(innerResult[0]=== "погода") {
+        getLocationCoordsCity();
+    } else {
+        cityWeatherName.innerText = innerResult;
+    }
+
+})
+
+recognition.start();
+recognition.addEventListener('end', recognition.start);
