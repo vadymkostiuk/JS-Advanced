b1f66c6b05b2b93c761d237bbc3b021db3375c7f
vadymkostyuk <kostiukvadym22@gmail.com>
2020-12-25 20:30:57 +0200

    task 4 done

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,16 +1,15 @@
-// 1. Navigation - done
-// 2. Add event listeners - done
-// 3. Add item - done
-// 4. Display item - done
-
-var itemsList = document.querySelector('.plates');
-var addItems = document.querySelector('.add-items');
-
+var itemsList = document.querySelector('.plates'),
+    addItems = document.querySelector('.add-items'),
+    food = document.querySelector( ".plates" ),
+    modalWindow = document.querySelector( "#modal-window" ),
+    btnYes = document.querySelector( "#yes"),
+    btnNo = document.querySelector( "#no");
 
 var items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(event) {
   event.preventDefault();
+
   var text = this.querySelector('[name=item]').value;
   var item = {
     text: text,
@@ -27,30 +26,106 @@ function populateList(plates, platesList) {
 
   platesList.innerHTML = plates.map(function (plate, index) {
     return `
-            <li>
-                <input type="checkbox" id="item${index}" class="check">
+            <li data-li-index="${index}" class="container-item">
+                <input data-index="${index}" type="checkbox" id="item${index}" class="check">
                 <label for="item${index}">${plate.text}</label>
+                <img class="edit-button" src="img/edit.png" alt="edit">
+                <img class="remove-button" src="img/delete.png" alt="delete">
+                <img class="save-button" src="img/save.png" alt="delete">
             </li>
         `
   }).join('');
-  // var input = document.querySelector('.plates li input').value;
-  // var p = document.createElement('p');
-  // input.appendChild(p);
-//   if(!input.checked) {
-//   console.log("111")
-// } else {
-//   console.log(input)
-// }
 
 }
 
 populateList(items, itemsList);
 
+function deleteItem (event) {
+  var targetData = event.target.parentNode.getAttribute('data-li-index');
+
+  if (event.target.className != 'remove-button' ) {
+    return;
+  }
+
+  modalWindow.style.display = "flex";
+
+  btnYes.addEventListener('click', function(){
+      event.target.parentNode.remove()
+      modalWindow.style.display = "none";
+      items.splice(targetData, 1);
+      localStorage.setItem('items', JSON.stringify(items));
+  });
+
+  btnNo.addEventListener('click', function () {
+    modalWindow.style.display = "none";
+  });
+
+}
+
+function editItem (event) {
+  if (event.target.className != 'edit-button' ) {
+    return;
+  }
+
+  event.target.parentNode.children[1].setAttribute("contenteditable", "true");
+  event.target.parentNode.children[1].focus();
+  event.target.style.display = "none";
+  event.target.parentNode.children[4].style.display = "block"
+  event.target.parentNode.children[3].style.order = "1"
+
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
+function saveItem (event) {
+  var itemId = event.target.parentNode.children[0].id,
+      innerValue = event.target.parentNode.children[1].innerText,
+      numId = parseInt(itemId.match(/\d+/));
+
+  if (event.target.className != 'save-button' ) {
+    return;
+  }
+
+  event.target.parentNode.children[1].setAttribute("contenteditable", "false");
+  event.target.style.display = "none";
+  event.target.parentNode.children[2].style.display = "block";
+
+  items[numId].text = innerValue;
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
+function changeCheckbox (event) {
+
+  if (event.target.className != 'check' ) {
+    return;
+  }
+
+  if(event.target.checked) {
+    items[event.target.getAttribute('data-index')].done = true;
+    event.target.checked = true;
+    console.log()
+  } else {
+    items[event.target.getAttribute('data-index')].done = false;
+  }
+
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
+function checkCheckbox () {
+  var checkbox = document.querySelectorAll(".check");
+
+  checkbox.forEach(function(item,i){
+
+    if(items[i].done) {
+      item.checked = true;
+    }
+
+  })
+}
+
+checkCheckbox()
+
 addItems.addEventListener('submit', addItem);
-// var input = document.querySelectorAll('.plates li');
-// var p = document.createElement('p');
-// input.forEach(function (item) {
-//
-//   item.innerHTML = "212";
-//   console.log(item);
-// })
+food.addEventListener('click', deleteItem);
+food.addEventListener('click', editItem);
+food.addEventListener('click', saveItem);
+food.addEventListener('click', changeCheckbox);
